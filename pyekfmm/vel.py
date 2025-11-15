a3eb8a8e94bdf202296bb7fdc52b81e44f7969eb
Yangkang Chen <chenyk@BEGs-MacBook-Pro.local>
2025-11-14 22:03:38 -0600

    Fri Nov 14 22:03:38 CST 2025

--- pyekfmm/vel.py
+++ pyekfmm/vel.py
@@ -27,3 +27,38 @@ def vgrad(v1,v2,n1,n2):
 	vel=np.matmul(np.linspace(v1,v2,n1)[:,np.newaxis],np.ones([1,n2]))
 	
 	return vel
+	
+def vgrad3d(v1,v2,n1,n2,n3):
+	'''
+	vgrad: velocity model with gradient increase
+	
+	INPUT
+	v1,v2: starting and ending velocities
+	n1,n2,n3: axis size (z,x,y)
+	
+	OUTPUT
+	vel
+	
+	EXAMPLE
+	from pyekfmm import vgrad3d
+	import matplotlib.pyplot as plt
+	vel=vgrad3d(3,5,50,40,30)
+	from pyseistr import plot3d
+	plot3d(vel,dz=1,dx=1,dy=1,cmap=plt.jet(),barlabel='Velocity (km/s)',showf=False,close=False)
+	plt.title('Velocity model of linear increase with depth')
+	plt.gca().set_xlabel("X (sample)",fontsize='large', fontweight='normal')
+	plt.gca().set_ylabel("Y (sample)",fontsize='large', fontweight='normal')
+	plt.gca().set_zlabel("Z (sample)",fontsize='large', fontweight='normal')
+	plt.show()
+	
+	'''
+	
+	import numpy as np
+	
+	vel=np.matmul(np.linspace(v1,v2,n1)[:,np.newaxis],np.ones([1,n2]))
+	
+	vel3d=np.zeros([n1,n2,n3])
+	for ii in range(n3):
+		vel3d[:,:,ii]=vel
+	
+	return vel3d
