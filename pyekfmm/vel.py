1cc9c6d0099d9f29931dfd01cf088cf881450e0f
Yangkang Chen <chenyk@BEGs-MacBook-Pro.local>
2025-11-14 22:25:54 -0600

    Fri Nov 14 22:25:54 CST 2025

--- pyekfmm/vel.py
+++ pyekfmm/vel.py
@@ -44,7 +44,7 @@ def vgrad3d(v1,v2,n1,n2,n3):
 	import matplotlib.pyplot as plt
 	vel=vgrad3d(3,5,50,40,30)
 	from pyseistr import plot3d
-	plot3d(vel,dz=1,dx=1,dy=1,cmap=plt.jet(),barlabel='Velocity (km/s)',showf=False,close=False)
+	plot3d(vel,dz=1,dx=1,dy=1,cmap=plt.jet(),barlabel='Velocity (km/s)',ifnewfig=False,showf=False,close=False)
 	plt.title('Velocity model of linear increase with depth')
 	plt.gca().set_xlabel("X (sample)",fontsize='large', fontweight='normal')
 	plt.gca().set_ylabel("Y (sample)",fontsize='large', fontweight='normal')
